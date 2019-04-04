d1f5660eee55740522763ce4f51b0977e544165f
Dana Simmons <dana@simmons-audio.com>
2019-04-04 19:55:58 -0400

    basic grid works, trouble with image requires

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import '../App.css';
 import Nav from './Nav'
 import hogs from '../porkers_data';
+import HogCard from './HogCard'
 
 class App extends Component {
+  state = {
+                hogs: hogs
+              }
+  
   render() {
     return (
       <div className="App">
           < Nav />
-
+          <div className="indexWrapper">
+            {/* <Filters /> */}
+            <ul>
+              {this.state.hogs.map((hog)=><li><HogCard hog={hog} /></li>)}
+            </ul>
+          </div>  
       </div>
     )
   }
---
+++ src/components/HogCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function HogCard(props) {
+    const {name, medal, specialty, greased, weight, img} = props.hog
+    console.log('img', img)
+    let hogImg = require(`${img}`)
+  return (
+    <div className="pigTile">
+        <div className="image">
+            <img src={hogImg} alt="hog" />
+        </div>
+        <div className="content">
+            <h3>{name}</h3>
+            <div>
+                Higest Medal: {medal}
+            </div>
+            <div className="normalText">
+                Specialty: {specialty}
+            </div>
+            <div>
+                {greased? "Greased" : "Non-Greased"}
+            </div>
+            <div>
+                Weight: {weight}
+            </div>
+        </div>
+    </div>
+  )
+}
